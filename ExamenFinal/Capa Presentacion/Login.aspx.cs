d9fc5a987af0203a8a80599646846b0436dedb4a
Crimcat <adriangutcarb@gmail.com>
2024-04-26 20:28:08 -0600

    examen

---
+++ ExamenFinal/Capa Presentacion/Login.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ExamenFinal.CapaDatos;
+
+namespace ExamenFinal.Presentacion
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected System.Void Page_Load(System.Object sender, System.EventArgs e)
+        {
+            ClsAgente oAgente = new ClsAgente();
+
+            username.input = oAgente.Nombre;
+
+            password.input = oAgente.Contra;
+
+
+        }
+
+    }
+}
